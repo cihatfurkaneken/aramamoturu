d67cb7b0bf496a7c79a6c991366251c07f288373
Cihat Furkan <48259639+cihatfurkaneken@users.noreply.github.com>
2020-01-01 02:27:55 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -13,6 +13,15 @@
 
 
 using namespace std;
+/*400.000 kelime okunur ve listelere harf sayılarına göre dizilir. Ararken arananın harf sayısına göre listelere bakar ve
+eşleşen'i bulup sayisini ve hangi dosyada kaç tane olduğunu yazar. Tam eşleşme bulamazsa arananın harf sayısından
+2 alt ve 2 üst listede levenshtein derecesi 2 ve altı olanları bulur ve yazar.
+
+pdfdoc.exe: metin2.pdf ve metin4.docx'i txt formatına çevirip okur,okuduktan sonra txt,ler silinir. 
+SharpZipLib.dll ve itextsharp.dll bu exenin dosyalarıdır. Classları içerir
+
+distance.h : Levenshtein distance değerlerini bulur levenshtein algoritmasının kütüphanesidir.
+*/
 
 
 struct node{
